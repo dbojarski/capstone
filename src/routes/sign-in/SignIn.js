f021d79fc11f151b0bb248da81d9e9c86b5474fa
dbojarski <dbojarski@pgs-soft.com>
2022-08-04 22:07:51 +0200

    Firebase setting up

--- src/routes/sign-in/SignIn.js
+++ src/routes/sign-in/SignIn.js
@@ -1,3 +1,18 @@
+import {
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+} from '../../utils/firebase/firebase.utils';
+
 export function SignIn() {
-  return 'Sign in page';
+  const logIn = async () => {
+    const { user } = await signInWithGooglePopup();
+
+    await createUserDocumentFromAuth(user);
+  };
+  return (
+    <>
+      <h1>Sign in page</h1>
+      <button onClick={logIn}>Sign in</button>
+    </>
+  );
 }
