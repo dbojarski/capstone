c656564f9bc1d1557842d4e07687017629265773
dbojarski <dbojarski@pgs-soft.com>
2022-09-06 13:22:36 +0200

    Simple express server to check heroku working

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('node:path');
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('*', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
