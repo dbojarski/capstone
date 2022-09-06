479e9d7d23d4ae629263661bda966edea20c2fed
dbojarski <dbojarski@pgs-soft.com>
2022-09-06 14:33:33 +0200

    Heroku postbuild

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const path = require('node:path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
