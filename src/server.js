e46533413e37e7e505c65eda235bdc49c7ae4ec2
dbojarski <dbojarski@pgs-soft.com>
2022-09-07 13:58:07 +0200

    Redirecting http requests to https

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('node:path');
 const app = express();
 const port = process.env.PORT || 8080;
+const enforce = require('express-sslify');
 
 app.use(express.static(path.join(__dirname, '/../build')));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../build', 'index.html'));
